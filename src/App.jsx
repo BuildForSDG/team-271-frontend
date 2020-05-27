51f8786b43b10bfac0ddb5e85953e0a55e702fc9
tomuhenry <tomusangehenry02@gmail.com>
2020-05-28 00:53:58 +0300

    edit test

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
 import Login from './components/login';
 import { Layout, Typography } from 'antd';
-import { orange, green } from '@ant-design/colors';
+import { orange } from '@ant-design/colors';
 const { Header, Footer, Content } = Layout;
 const { Title } = Typography;
 
 function App() {
   return (
-
     <Layout>
-      <Header style={{backgroundColor: orange.primary}}>
-      <Title level={2} className="logo">WONYA</Title>
+      <Header style={{ backgroundColor: orange.primary }}>
+        <Title level={2} className="logo">
+          WONYA
+        </Title>
       </Header>
       <Content>
         <Login />
       </Content>
-      <Footer>Wonya @2020</Footer>
-
+      <Footer style={{ backgroundColor: orange.primary }}>
+        <h3> Wonya @2020 </h3>
+      </Footer>
       <style global jsx>
-        {
-          `
-            .ant-layout-header h2 {
-              color: white;
-              padding-top: 8px;
-            }
-          `
-        }
+        {`
+          .ant-layout-header h2 {
+            color: #fff;
+            padding-top: 8px;
+          }
+        `}
       </style>
     </Layout>
   );
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Form, Input, Checkbox, Button, Row, Col, Typography } from "antd";
+import { Layout, Form, Input, Checkbox, Button, Row, Col, Typography } from 'antd';
 import 'antd/dist/antd.css';
 import { green } from '@ant-design/colors';
 import { FacebookFilled, GoogleOutlined, UserOutlined, LockOutlined } from '@ant-design/icons';
@@ -7,63 +7,53 @@ const { Content } = Layout;
 const { Title } = Typography;
 
 export default function Login() {
-
   return (
     <Content className="login-content">
-    <Row justify="center">
-      <Col xs={24} sm={22} md={10} lg={8} xl={8} >
-      <Title level={2}>SIGN IN</Title>
-      <Row justify="center" gutter={[8, 24]}>
-        <Col xs={22} sm={22} md={12} lg={12} xl={12}>
-        <Button className="facebook-btn btn" icon={<FacebookFilled />}>
-          Facebook
-        </Button>
-        </Col>
-        <Col xs={22} sm={22} md={12} lg={12} xl={12} >
-        <Button  className="google-btn btn" icon={<GoogleOutlined />}>
-          Google
-        </Button>
+      <Row justify="center">
+        <Col xs={24} sm={22} md={10} lg={8} xl={8}>
+          <Title level={2}>SIGN IN</Title>
+          <Row justify="center" gutter={[8, 24]}>
+            <Col xs={22} sm={22} md={12} lg={12} xl={12}>
+              <Button className="facebook-btn btn" icon={<FacebookFilled />}>
+                Facebook
+              </Button>
+            </Col>
+            <Col xs={22} sm={22} md={12} lg={12} xl={12}>
+              <Button className="google-btn btn" icon={<GoogleOutlined />}>
+                Google
+              </Button>
+            </Col>
+          </Row>
+          <Form name="basic">
+            <Form.Item name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
+              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+            </Form.Item>
+
+            <Form.Item name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
+              <Input.Password prefix={<LockOutlined className="site-form-item-icon" />} placeholder="Password" />
+            </Form.Item>
+
+            <Form.Item>
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <a className="login-form-forgot" href="/">
+                Forgot password
+              </a>
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="submit-button">
+                Sign In
+              </Button>
+            </Form.Item>
+          </Form>
         </Col>
       </Row>
-      <Form
-      name="basic"
-      >
-        <Form.Item
-          name="username"
-          rules={[{ required: true, message: 'Please input your username!' }]}
-        >
-          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-        </Form.Item>
 
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <Input.Password prefix={<LockOutlined className="site-form-item-icon" />} placeholder="Password" />
-        </Form.Item>
-
-        <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <a className="login-form-forgot" href="">
-          Forgot password
-        </a>
-        </Form.Item>
-
-        <Form.Item >
-          <Button type="primary" htmlType="submit" className="submit-button">
-            Sign In
-          </Button>
-        </Form.Item>
-      </Form>
-      </Col>
-    </Row>
-
-    <style jsx>
-      {
-        `
+      <style jsx>
+        {`
           .login-content {
             padding: 100px;
           }
@@ -89,8 +79,8 @@ export default function Login() {
           }
 
           .google-btn {
-            background-color: #DB4437;
-            border-color: #DB4437;
+            background-color: #db4437;
+            border-color: #db4437;
           }
 
           .google-btn:hover {
@@ -102,9 +92,8 @@ export default function Login() {
           .login-form-forgot {
             float: right;
           }
-        `
-      }
-    </style>
+        `}
+      </style>
     </Content>
   );
 }
