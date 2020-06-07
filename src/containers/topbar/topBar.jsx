9021de58c46ee5b291745877a8f4174c2cabe39d
Andrew Musali <andrewmusali@gmail.com>
2020-06-07 20:45:23 +0300

    fixes yarn.lock error

--- src/containers/topbar/topBar.jsx
+++ src/containers/topbar/topBar.jsx
@@ -7,7 +7,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function TopBar(){
     const [show, setShow] = useState(false);
-
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     return(
